87d9416196437dc7f601429d8d704dd73a7409ae
Nicholas Curran <nicholas@curran.tech>
2025-09-09 00:46:54 -0400

    modified:   modules/recon/domains-contacts/whois_pocs.py
    	modified:   modules/recon/domains-contacts/whois_pocs.py

--- modules/recon/domains-contacts/whois_pocs.py
+++ modules/recon/domains-contacts/whois_pocs.py
@@ -1,5 +1,5 @@
 from recon.core.module import BaseModule
-
+import pdb
 
 class Module(BaseModule):
 
@@ -7,7 +7,7 @@ class Module(BaseModule):
         'name': 'Whois POC Harvester',
         'author': 'Tim Tomes (@lanmaster53)',
         'version': '1.1',
-        'description': ('Uses the ARIN Whois RWS to harvest POC data'
+        'description': ('Uses the ARIN Whois RWS to harvest POC data '
                         'from whois queries for the given domain. '
                         'Updates the \'contacts\' table with the results.'),
         'query': ('SELECT DISTINCT domain '
@@ -24,23 +24,33 @@ class Module(BaseModule):
             if 'Sorry, there were no results.' in resp.text:
                 self.output('No contacts found.')
                 continue
-            handles = [x['@handle'] for x in resp.json()['pocs']['pocRef']] if type(resp.json()['pocs']['pocRef']) == list else [resp.json()['pocs']['pocRef']['@handle']]
+            handles = ([x['@handle'] for x in resp.json()['pocs']['pocRef']]
+                       if isinstance(resp.json()['pocs']['pocRef'], list)
+                       else [resp.json()['pocs']['pocRef']['@handle']])
             for handle in handles:
                 url = f"http://whois.arin.net/rest/poc/{handle}"
                 self.verbose(f"URL: {url}")
                 resp = self.request('GET', url, headers=headers)
                 poc = resp.json()['poc']
-                emails = poc['emails']['email'] \
-                    if type(poc['emails']['email']) == list else [poc['emails']['email']]
+                emails = (poc['emails']['email']
+                          if isinstance(poc['emails']['email'], list)
+                          else [poc['emails']['email']])
                 for email in emails:
-                    fname = poc['firstName']['$'] if 'firstName' in poc else None
+                    fname = (poc['firstName']['$']
+                             if 'firstName' in poc else None)
                     lname = poc['lastName']['$']
                     name = ' '.join([x for x in [fname, lname] if x])
                     email = email['$']
                     title = 'Whois contact'
                     city = poc['city']['$'].title()
-                    state = poc['iso3166-2']['$'].upper() if 'iso3166-2' in poc else None
+                    state = (poc['iso3166-2']['$'].upper()
+                             if 'iso3166-2' in poc else None)
                     region = ', '.join([x for x in [city, state] if x])
                     country = poc['iso3166-1']['name']['$'].title()
                     if email.lower().endswith(domain.lower()):
-                        self.insert_contacts(first_name=fname, last_name=lname, email=email, title=title, region=region, country=country)
+                        self.insert_contacts(first_name=fname,
+                                             last_name=lname,
+                                             email=email,
+                                             title=title,
+                                             region=region,
+                                             country=country)
